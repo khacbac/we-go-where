c993fddbedf923103d02c25e39cc1fbe680718d3
khacbac <hokhacbac@gmail.com>
2023-11-23 22:28:27 +0700

    remove unused codes

--- src/features/add-card/AddCardScreen.tsx
+++ src/features/add-card/AddCardScreen.tsx
@@ -5,42 +5,12 @@ import { AppButton, AppContainer } from "../../components";
 //@ts-ignore
 import Omise from "omise-react-native";
 import { AppNavigationProps } from "../../types";
-import { CalculatorStore } from "../../stores/calculator-store";
-import { autorun } from "mobx";
 import { useStore } from "../../stores";
 Omise.config("pkey_test_5wvisbxphp1zapg8ie6", "2017-11-12");
-
-const source = {
-  //   html: `<form id="checkoutForm" method="POST" action="/charge">
-  //   <script type="text/javascript" src="https://cdn.omise.co/omise.js"
-  //           data-key="OMISE_PUBLIC_KEY"
-  //           data-amount="12345"
-  //           data-currency="THB"
-  //           data-default-payment-method="credit_card">
-  //   </script>
-  // </form>`,
-  // uri: a,
-};
-
-// const customHTMLElementModels = {
-//   "blue-circle": HTMLElementModel.fromCustomModel({
-//     tagName: "blue-circle",
-//     mixedUAStyles: {
-//       width: 50,
-//       height: 50,
-//       borderRadius: 25,
-//       alignSelf: "center",
-//       backgroundColor: "blue",
-//     },
-//     contentModel: HTMLContentModel.block,
-//   }),
-// };
 
 type IProps = AppNavigationProps<"CardsList"> & {};
 const AddCardScreen: React.FC<IProps> = ({}) => {
   const { todoStore } = useStore();
-  const { width } = useWindowDimensions();
-  console.log("BACHK_____ called : ");
 
   const creatAToken = async () => {
     try {
@@ -58,11 +28,7 @@ const AddCardScreen: React.FC<IProps> = ({}) => {
           country: "US",
         },
       });
-
-      console.log("data", JSON.stringify(data));
-    } catch (error) {
-      console.log("BACHK_____ error : ", error);
-    }
+    } catch (error) {}
   };
 
   const creatACustomer = async () => {
@@ -72,23 +38,16 @@ const AddCardScreen: React.FC<IProps> = ({}) => {
       });
 
       console.log("data", JSON.stringify(data));
-    } catch (error) {
-      console.log("BACHK_____ error : ", error);
-    }
+    } catch (error) {}
   };
 
   const onChangeTitle = async () => {
-    console.log("BACHK_____ todoStore : ", todoStore);
     todoStore.changeTitle();
   };
 
   return (
     <AppContainer renderToHardwareTextureAndroid={true}>
       <Text>AddCardScreen</Text>
-      <Text>{`Price: ${todoStore.title}`}</Text>
-      <AppButton text="BACK" />
-      <AppButton text="Create Token" onPress={creatAToken} />
-      <AppButton text="Create Customer" onPress={creatACustomer} />
       <AppButton text="Change Todo Title" onPress={onChangeTitle} />
     </AppContainer>
   );
